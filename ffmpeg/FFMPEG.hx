8f8e9911560ddebd8004b34a6f69f6334b944f98
soywiz <soywiz@gmail.com>
2014-01-07 20:28:02 +0100

    - First working version

--- ffmpeg/FFMPEG.hx
+++ ffmpeg/FFMPEG.hx
@@ -1,16 +1,180 @@
 package ffmpeg;
 
+import flash.media.SoundChannel;
+import flash.events.Event;
+import flash.events.EventDispatcher;
+import haxe.Timer;
+import flash.display.PixelSnapping;
+import flash.display.Bitmap;
+import flash.display.Sprite;
+import flash.events.SampleDataEvent;
+import flash.utils.Endian;
+import sys.io.FileOutput;
+import haxe.io.Output;
+import haxe.io.BytesData;
+import haxe.Log;
+import flash.display.BitmapData;
 import haxe.io.Bytes;
 import flash.utils.ByteArray;
 import flash.media.Sound;
 import sys.FileSystem;
 import sys.io.File;
+import cpp.Lib;
 
-class FFMPEG {
-	public static function getVersion():String {
+class FFMPEG
+{
+	public function new()
+	{
+	}
+
+	public static function getVersion():String
+	{
 		return hx_ffmpeg_get_version();
 	}
 
-	static var hx_ffmpeg_get_version = cpp.Lib.load("openfl-ffmpeg", "hx_ffmpeg_get_version", 0);
-	
+	public var width(default, null):Int;
+	public var height(default, null):Int;
+	public var bitmapData(default, null):BitmapData;
+	private var buffer:ByteArray;
+	private var sound:Sound;
+	private var soundChannel:SoundChannel;
+	//private var _tempOutput:FileOutput;
+	private var soundBuffer:ConsumerProducerBuffer<Float>;
+	private var completed:Bool;
+	private var onComplete:Void -> Void;
+
+	public function openAndPlay(fileName:String, onComplete:Void -> Void = null):Void
+	{
+		if (bitmapData != null) dispose();
+
+		hx_ffmpeg_open_file(fileName);
+		this.completed = false;
+		this.width = hx_ffmpeg_get_width();
+		this.height = hx_ffmpeg_get_height();
+		this.bitmapData = new BitmapData(width, height, true, 0x00000000);
+		this.buffer = new ByteArray(width * height * 4);
+		this.sound = new Sound();
+		this.soundBuffer = new ConsumerProducerBuffer<Float>();
+		this.onComplete = onComplete;
+
+		this.sound.addEventListener(SampleDataEvent.SAMPLE_DATA, generateSound);
+		this.soundChannel = this.sound.play();
+
+		var timer = new Timer(1000 / 30);
+		timer.run = function() {
+			decode_frame();
+			if (completed) {
+				timer.stop();
+				dispose();
+			}
+		};
+	}
+
+	private function dispose():Void
+	{
+		hx_ffmpeg_close_file();
+		this.bitmapData = null;
+		this.buffer = null;
+		if (this.soundChannel)
+		{
+			this.soundChannel.stop();
+			this.soundChannel = null;
+		}
+	}
+
+	public function stop()
+	{
+		completed = true;
+	}
+
+	private function generateSound(event:SampleDataEvent)
+	{
+		var bufferSize = 8192 * 2 * 4;
+
+		if (soundBuffer.available == 0 && completed) return;
+
+		while (event.data.length < bufferSize)
+		{
+			event.data.writeFloat((soundBuffer.available > 0) ? soundBuffer.read() : 0);
+		}
+	}
+
+	public function decode_frame():FFMPEG
+	{
+		buffer.position = 0;
+		this.completed = hx_ffmpeg_decode_frame(buffer.getData(), emit_sound) != 0;
+		bitmapData.lock();
+		bitmapData.setPixels(bitmapData.rect, buffer);
+		bitmapData.unlock();
+
+		if (this.completed && onComplete != null) {
+			onComplete();
+		}
+
+		return this;
+	}
+
+	static private function getByteArrayFromBytesData(bytesData:BytesData):ByteArray
+	{
+		var byteArray:ByteArray = new ByteArray();
+		byteArray.endian = Endian.LITTLE_ENDIAN;
+		for (n in 0 ... bytesData.length) byteArray.writeByte(cast bytesData[n]);
+		byteArray.position = 0;
+		return byteArray;
+	}
+
+	private function emit_sound(channels:Array<BytesData>)
+	{
+		var nsamples = Std.int(channels[0].length / 2);
+
+		var channelLByteArray = getByteArrayFromBytesData(channels[0]);
+		var channelRByteArray = getByteArrayFromBytesData(channels[1]);
+
+		for (n in 0 ... nsamples)
+		{
+			var l = channelLByteArray.readShort() / 32767;
+			var r = channelRByteArray.readShort() / 32767;
+			soundBuffer.write(l);
+			soundBuffer.write(r);
+		}
+	}
+
+	private static var hx_ffmpeg_get_version = Lib.load("openfl-ffmpeg", "hx_ffmpeg_get_version", 0);
+	private static var hx_ffmpeg_open_file = Lib.load("openfl-ffmpeg", "hx_ffmpeg_open_file", 1);
+	private static var hx_ffmpeg_get_width = Lib.load("openfl-ffmpeg", "hx_ffmpeg_get_width", 0);
+	private static var hx_ffmpeg_get_height = Lib.load("openfl-ffmpeg", "hx_ffmpeg_get_height", 0);
+	private static var hx_ffmpeg_decode_frame = Lib.load("openfl-ffmpeg", "hx_ffmpeg_decode_frame", 2);
+	private static var hx_ffmpeg_close_file = Lib.load("openfl-ffmpeg", "hx_ffmpeg_close_file", 0);
+}
+
+class ConsumerProducerBuffer<T>
+{
+	private var data:Array<T>;
+	public var available(get, never):Int;
+	private var readPosition:Int = 0;
+
+	public function new()
+	{
+		data = [];
+	}
+
+	private function get_available():Int
+	{
+		return data.length - readPosition;
+	}
+
+	public function read():T
+	{
+		if (readPosition >= 0x4000) {
+			this.data = this.data.slice(0x4000);
+			this.readPosition -= 0x4000;
+		}
+		//Array.slice
+		return this.data[readPosition++];
+	}
+
+	public function write(value:T):Void
+	{
+		this.data.push(value);
+	}
 }
--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -7,6 +7,7 @@
 #endif
 
 #include <hx/CFFI.h>
+//#include <hx/CFFIAPI.h>
 
 #define DEFINE_FUNC(COUNT, NAME, ...) value NAME(__VA_ARGS__); DEFINE_PRIM(NAME, COUNT); value NAME(__VA_ARGS__)
 #define DEFINE_FUNC_0(NAME) DEFINE_FUNC(0, NAME)
@@ -21,6 +22,7 @@ extern "C" {
 	#include <libavcodec/avcodec.h>
 	#include <libavformat/avformat.h>
 	#include <libswscale/swscale.h>
+	#include <libswresample/swresample.h>
 }
 
 #include <stdio.h>
@@ -38,13 +40,20 @@ void __check_init()
 
 typedef struct {
 	AVFormatContext *pFormatCtx;
-	AVCodecContext  *pCodecCtx;
-	AVCodec         *pCodec;
-	AVDictionary    *optionsDict;
+	
+	AVCodecContext  *videoCodecContext;
+	AVCodec         *videoCodec;
+	AVDictionary    *videoOptionsDict;
+	
+	AVCodecContext  *audioCodecContext;
+	AVCodec         *audioCodec;
+	AVDictionary    *audioOptionsDict;
+
 	AVFrame         *pFrame;
 	AVFrame         *pFrameRGB;
 	uint8_t         *buffer;
 	int videoStream;
+	int audioStream;
 	
 	struct SwsContext      *sws_ctx;
 	int numBytes;
@@ -63,42 +72,47 @@ int __ffmpeg_open_file(FfmpegContext *context, const char *name)
 	av_dump_format(context->pFormatCtx, 0, name, 0);
 	
 	context->videoStream=-1;
+	context->audioStream=-1;
 	for(int i=0; i<context->pFormatCtx->nb_streams; i++)
 	{
 		if(context->pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-			context->videoStream=i;
-			break;
+			if (context->videoStream < 0) context->videoStream=i;
+		}
+
+		if(context->pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_AUDIO) {
+			if (context->audioStream < 0) context->audioStream=i;
 		}
 	}
 	
-	if(context->videoStream==-1) return -1; // Didn't find a video stream
+	if (context->videoStream == -1) return -1; // Didn't find a video stream
+	if (context->audioStream == -1) return -1; // Didn't find a video stream
 	
-	context->pCodecCtx=context->pFormatCtx->streams[context->videoStream]->codec;
+	context->videoCodecContext = context->pFormatCtx->streams[context->videoStream]->codec;
+	context->videoCodec=avcodec_find_decoder(context->videoCodecContext->codec_id);
+	if (context->videoCodec==NULL) return -3; // Codec not found
+	if (avcodec_open2(context->videoCodecContext, context->videoCodec, &context->videoOptionsDict)<0) return -4; // Could not open codec
 	
-	context->pCodec=avcodec_find_decoder(context->pCodecCtx->codec_id);
-	if(context->pCodec==NULL) {
-		return -3; // Codec not found
-	}
+	context->audioCodecContext = context->pFormatCtx->streams[context->audioStream]->codec;
+	context->audioCodec = avcodec_find_decoder(context->audioCodecContext->codec_id);
+	if (context->audioCodec==NULL) return -3; // Codec not found
+	if (avcodec_open2(context->audioCodecContext, context->audioCodec, &context->audioOptionsDict)<0) return -4; // Could not open codec
 	
-	if(avcodec_open2(context->pCodecCtx, context->pCodec, &context->optionsDict)<0) return -4; // Could not open codec
 	
 	// Allocate video frame
-	context->pFrame=avcodec_alloc_frame();
-
 	// Allocate an AVFrame structure
-	context->pFrameRGB=avcodec_alloc_frame();
-	if(context->pFrameRGB==NULL) return -1;
+	context->pFrame = avcodec_alloc_frame();
+	context->pFrameRGB = avcodec_alloc_frame();
 	
 	// Determine required buffer size and allocate buffer
-	context->numBytes=avpicture_get_size(USED_PIX_FMT, context->pCodecCtx->width, context->pCodecCtx->height);
-	context->buffer=(uint8_t *)av_malloc(context->numBytes*sizeof(uint8_t));
+	context->numBytes = avpicture_get_size(USED_PIX_FMT, context->videoCodecContext->width, context->videoCodecContext->height);
+	context->buffer = (uint8_t *)av_malloc(context->numBytes*sizeof(uint8_t));
 
-	context->sws_ctx = sws_getContext(context->pCodecCtx->width, context->pCodecCtx->height, context->pCodecCtx->pix_fmt, context->pCodecCtx->width, context->pCodecCtx->height, USED_PIX_FMT, SWS_BILINEAR, NULL, NULL, NULL);
+	context->sws_ctx = sws_getContext(context->videoCodecContext->width, context->videoCodecContext->height, context->videoCodecContext->pix_fmt, context->videoCodecContext->width, context->videoCodecContext->height, USED_PIX_FMT, SWS_BILINEAR, NULL, NULL, NULL);
 
 	// Assign appropriate parts of buffer to image planes in pFrameRGB
 	// Note that pFrameRGB is an AVFrame, but AVFrame is a superset
 	// of AVPicture
-	avpicture_fill((AVPicture *)context->pFrameRGB, context->buffer, USED_PIX_FMT, context->pCodecCtx->width, context->pCodecCtx->height);
+	avpicture_fill((AVPicture *)context->pFrameRGB, context->buffer, USED_PIX_FMT, context->videoCodecContext->width, context->videoCodecContext->height);
 
 	return 0;
 }
@@ -113,7 +127,7 @@ void __fmpeg_close_file(FfmpegContext *context)
 	av_free(context->pFrame);
 
 	// Close the codec
-	avcodec_close(context->pCodecCtx);
+	avcodec_close(context->videoCodecContext);
 
 	// Close the video file
 	avformat_close_input(&context->pFormatCtx);
@@ -130,40 +144,90 @@ void __fmpeg_copy_frame_to_pointer(AVFrame *pFrame, int width, int height, char 
 	}
 }
 
-void __fmpeg_decode_frame(FfmpegContext *context, char *output_ptr, int output_len)
+int __fmpeg_decode_frame(FfmpegContext *context, char *output_ptr, int output_len, value emit_sound_callback)
 {
 	AVPacket        packet;
 	int frameFinished;
-  while(av_read_frame(context->pFormatCtx, &packet)>=0) {
-    // Is this a packet from the video stream?
-    if(packet.stream_index==context->videoStream) {
-      // Decode video frame
-      avcodec_decode_video2(context->pCodecCtx, context->pFrame, &frameFinished, 
-                           &packet);
-      
-      // Did we get a video frame?
-      if(frameFinished) {
-        // Convert the image from its native format to RGB
-        sws_scale
-        (
-            context->sws_ctx,
-            (uint8_t const * const *)context->pFrame->data,
-            context->pFrame->linesize,
-            0,
-            context->pCodecCtx->height,
-            context->pFrameRGB->data,
-            context->pFrameRGB->linesize
-        );
-        
+	int generated_frame = 0;
+	while(av_read_frame(context->pFormatCtx, &packet)>=0)
+	{
+		// Is this a packet from the video stream?
+		if (packet.stream_index==context->videoStream) {
+			// Decode video frame
+			avcodec_decode_video2(context->videoCodecContext, context->pFrame, &frameFinished, &packet);
+
+			// Did we get a video frame?
+			if (frameFinished) {
+				// Convert the image from its native format to RGB
+				sws_scale(
+					context->sws_ctx,
+					(uint8_t const * const *)context->pFrame->data,
+					context->pFrame->linesize,
+					0,
+					context->videoCodecContext->height,
+					context->pFrameRGB->data,
+					context->pFrameRGB->linesize
+				);
+
+				__fmpeg_copy_frame_to_pointer(context->pFrameRGB, context->videoCodecContext->width, context->videoCodecContext->height, output_ptr, output_len);
+				generated_frame = 1;
+			}
+		} else if (packet.stream_index == context->audioStream) {
+			static AVFrame audioFrame;
+			int got_frame = 0;
+			int len1 = avcodec_decode_audio4(context->audioCodecContext, &audioFrame, &got_frame, &packet);
+
+			if (got_frame)
+			{
+			
+				// frame_size
+				
+				//printf("%d, %d, %d\n", context->audioCodecContext->sample_rate, context->audioCodecContext->channels, context->audioCodecContext->sample_fmt);
+
+				int in_nchannels = context->audioCodecContext->channels;
+				int in_nsamples = audioFrame.nb_samples;
+				AVSampleFormat in_sample_format = context->audioCodecContext->sample_fmt;
+				int in_channel_layout = context->audioCodecContext->channel_layout;
+				
+				int in_size = av_samples_get_buffer_size(NULL, in_nchannels, in_nsamples, in_sample_format, 1);
+				int out_size = in_size / in_nchannels;
+				
+				value array = alloc_array(0);
+				for (int n = 0; n < in_nchannels; n++) {
+					buffer out_buffer = alloc_buffer_len(out_size);
+					memcpy((uint8_t *)buffer_data(out_buffer), audioFrame.data[n], out_size);
+					val_array_push(array, buffer_val(out_buffer));
+				}
+
+			/*
+				SwrContext *swr = swr_alloc_set_opts(
+					NULL,
+					AV_CH_LAYOUT_STEREO, AV_SAMPLE_FMT_FLT, 44100,
+					context->audioCodecContext->channel_layout, context->audioCodecContext->sample_fmt, context->audioCodecContext->sample_rate,
+					0, NULL
+				);
+
+				int in_size = av_samples_get_buffer_size(NULL, in_nchannels, in_nsamples, in_sample_format, 1);
+				int out_size = av_samples_get_buffer_size(NULL, in_nchannels, in_nsamples, AV_SAMPLE_FMT_FLT, 1);
+				
+				buffer sound_buffer = alloc_buffer_len(out_size * 10);
+				
+				uint8_t *out = (uint8_t *)buffer_data(sound_buffer);
+				
+				//const uint8_t *in = (uint8_t *)audioFrame.data[0];
+				
+				//swr_convert(swr, &out, nsamples / 10, (const uint8_t **)audioFrame.data, nsamples);
+				
+				swr_free(&swr);
+				*/
+
+				val_call1(emit_sound_callback, array);
+			}
+		}
 		av_free_packet(&packet);
-		__fmpeg_copy_frame_to_pointer(context->pFrameRGB, context->pCodecCtx->width, context->pCodecCtx->height, output_ptr, output_len);
-		return;
-      }
-    } else {
-		av_free_packet(&packet);
+		if (generated_frame) return 0;
 	}
-    
-  }
+	return 1;
 }
 
 DEFINE_FUNC_0(hx_ffmpeg_get_version)
@@ -173,26 +237,26 @@ DEFINE_FUNC_0(hx_ffmpeg_get_version)
 }
 
 
-DEFINE_FUNC_0(hx_ffmpeg_open_file)
+DEFINE_FUNC_1(hx_ffmpeg_open_file, file_name)
 {
 	__check_init();
-	__ffmpeg_open_file(&ffmpeg_context, "c:/temp/test.mpg");
+	__ffmpeg_open_file(&ffmpeg_context, val_get_string(file_name));
 	return alloc_null();
 }
 
 DEFINE_FUNC_0(hx_ffmpeg_get_width)
 {
 	__check_init();
-	return alloc_int(ffmpeg_context.pCodecCtx->width);
+	return alloc_int(ffmpeg_context.videoCodecContext->width);
 }
 
 DEFINE_FUNC_0(hx_ffmpeg_get_height)
 {
 	__check_init();
-	return alloc_int(ffmpeg_context.pCodecCtx->height);
+	return alloc_int(ffmpeg_context.videoCodecContext->height);
 }
 
-DEFINE_FUNC_1(hx_ffmpeg_decode_frame, data_buffer_value)
+DEFINE_FUNC_2(hx_ffmpeg_decode_frame, data_buffer_value, emit_sound_callback)
 {
 	__check_init();
 	
@@ -205,14 +269,14 @@ DEFINE_FUNC_1(hx_ffmpeg_decode_frame, data_buffer_value)
 	char *data_buffer_ptr = buffer_data(data_buffer);
 	int data_buffer_len = buffer_size(data_buffer);
 	
-	__fmpeg_decode_frame(&ffmpeg_context, data_buffer_ptr, data_buffer_len);
-	
-	return alloc_null();
+	return alloc_int(__fmpeg_decode_frame(&ffmpeg_context, data_buffer_ptr, data_buffer_len, emit_sound_callback));
 }
 
 DEFINE_FUNC_0(hx_ffmpeg_close_file)
 {
 	__check_init();
 	__fmpeg_close_file(&ffmpeg_context);
+	//value vv;
+	//val_call1(vv);
 	return alloc_null();
 }
